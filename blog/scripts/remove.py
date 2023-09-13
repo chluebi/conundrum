f459a3d4953b141e1e162fac83ad7ffa36a22453
chluebi <42419603+chluebi@users.noreply.github.com>
2023-09-13 12:18:07 +0200

    better git changes

--- blog/scripts/remove.py
+++ blog/scripts/remove.py
@@ -4,8 +4,8 @@ from remove_no_git import *
 
 
 if changed:
-    os.system(f'git add {md_destination_path}/*')
-    os.system(f'git add {media_destination_path}/*')
+    os.system(f'git add {md_destination_path}/')
+    os.system(f'git add {media_destination_path}/')
     os.system(f'git commit -m \"removed article {compact_title}\"')
     os.system(f'git push origin')
 else:
